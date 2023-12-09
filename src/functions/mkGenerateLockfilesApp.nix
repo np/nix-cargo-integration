8dafbcc045d4a37223d218bc3756f587a52c60b3
Nicolas Pouillard <nicolas.pouillard@gmail.com>
2023-12-09 23:47:56 +0100

    WIP

--- src/functions/mkGenerateLockfilesApp.nix
+++ src/functions/mkGenerateLockfilesApp.nix
@@ -10,6 +10,10 @@ in
   pkgs.writeScript "generate-lockfiles" ''
     #!/usr/bin/env bash
     function addToGit() {
+      if [ ! -e "$1" ]; then
+        echo "Missing generated file $1"
+        exit 1
+      fi
       if [ -d ".git" ]; then
         git add $1
       fi
@@ -24,7 +28,10 @@ in
           cargoTomlPath = trimSlashes "${relPath}/Cargo.toml";
           cargoLockPath = trimSlashes "${relPath}/Cargo.lock";
         in ''
+          [ ! -e Cargo.lock ] || mv Cargo.lock Cargo.lock.bak
           ${buildToolchain}/bin/cargo generate-lockfile --manifest-path ${cargoTomlPath}
+          mv Cargo.lock ${cargoLockPath}
+          [ ! -e Cargo.lock.bak ] || mv Cargo.lock.bak Cargo.lock
           addToGit ${cargoLockPath}
         ''
       )
